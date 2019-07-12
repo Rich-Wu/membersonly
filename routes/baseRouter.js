9dcb91e051b80566379edcfac8862b8d8420a006
Richard <richie1988@gmail.com>
2019-07-12 14:28:21 -0400

    login form GET view done

--- routes/baseRouter.js
+++ routes/baseRouter.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
 
 // Root View
 router.get('/', (req, res, next) => {
-  res.render('index');
-})
+  res.render('index', {title: "Posts"});
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login_form', {title: "Login"});
+});
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: "/",
+    failureRediect: "/login"
+  })
+);
+
 
 module.exports = router;
